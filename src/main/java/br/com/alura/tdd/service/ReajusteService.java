2184ed520f209c94d1da53b11f589207f608fb23
Paulo Ferreira <phgferreira@outlook.com>
2022-09-23 09:03:34 -0300

    Aplicado o padrão de projeto Strategy

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,16 +8,8 @@ import br.com.alura.tdd.modelo.Funcionario;
 public class ReajusteService {
 
 	public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-		if (desempenho == Desempenho.A_DESEJAR) {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-			funcionario.reajustarSalario(reajuste);
-		} else if (desempenho == Desempenho.BOM) {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-			funcionario.reajustarSalario(reajuste);
-		} else if (desempenho == Desempenho.OTIMO) {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.2"));
-			funcionario.reajustarSalario(reajuste);
-		}
+		BigDecimal reajuste = funcionario.getSalario().multiply(desempenho.pegaReajusteSalarial());
+		funcionario.reajustarSalario(reajuste);
 	}
 
 }
