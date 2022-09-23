97dae4461746fb9529c6ba5a0826262557eb4547
Paulo Ferreira <phgferreira@outlook.com>
2022-09-23 12:39:24 -0300

    Substituindo a atribuição de bônus no valor zero para não atribuição com excessão IllegalArgumentException

--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -9,8 +9,8 @@ public class BonusService {
 
 	public BigDecimal calcularBonus(Funcionario funcionario) {
 		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
-		if (valor.compareTo(new BigDecimal("1000")) >= 0) {
-			valor = BigDecimal.ZERO;
+		if (valor.compareTo(new BigDecimal("10000")) >= 0) {
+			throw new IllegalArgumentException("Funcionário com salário maior que R$10.000 não pode receber o bônus");
 		}
 		return valor.setScale(2, RoundingMode.HALF_UP);
 	}
